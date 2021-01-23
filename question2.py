f0514d92ec1ece9c672773d5648f8584a45d5a36
Aman Kumar Singh <77712546+Singhaman75@users.noreply.github.com>
2021-01-23 20:34:06 +0530

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,23 @@
+'''Setup a dict structure like this in python
+Dict1: (this is a key, value pair of user id and username)
+{
+   “1” : “name1”,
+   “2” : “name2”,
+   “3” : “name3”
+} etc.. '''
+
+num_of_users=int(input("enter no. of users"))
+dict1,dict2,count={},{},1
+for i in range(0,num_of_users):
+    name=input("enter user name")
+    score=int(input("enter exam score"))
+    dict1.update({count:name})
+    dict2.update({count:score})
+    count=count+1
+list1,count=[],1
+for i in range(0,num_of_users):
+    list1.append(dict2.get(count))
+    count=count+1
+value1=max(list1)
+key1=list(dict2.keys())[list(dict2.values()).index(value1)]
+print(f"{key1}:{value1}")
