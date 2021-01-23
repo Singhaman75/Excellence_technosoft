1f05927b1c7244d8b269e5941fcab0304e545c39
Aman Kumar Singh <77712546+Singhaman75@users.noreply.github.com>
2021-01-23 19:59:00 +0530

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,15 @@
+m=int(input("enter no. of users"))
+x,y,a={},{},1
+for i in range(0,m):
+    b=input("enter user name")
+    c=int(input("enter exam score"))
+    x.update({a:b})
+    y.update({a:c})
+    a=a+1
+n,a=[],1
+for i in range(0,m):
+    n.append(y.get(a))
+    a=a+1
+p=max(n)
+answer=list(y.keys())[list(y.values()).index(p)]
+print(f"{answer}:{p}")
